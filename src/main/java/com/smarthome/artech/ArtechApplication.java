076c2ce3e50d8ac4a6cfce60db58947742d80aba
hieunv2 <vanhieu13546@gmail.com>
2020-08-07 15:03:05 +0700

    Add data user

--- src/main/java/com/smarthome/artech/ArtechApplication.java
+++ src/main/java/com/smarthome/artech/ArtechApplication.java
@@ -9,9 +9,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class ArtechApplication{
+public class ArtechApplication implements CommandLineRunner{
 
     public static void main(String[] args) {
         SpringApplication.run(ArtechApplication.class, args);
+    }
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Khi chương trình chạy
+        // Insert vào csdl một user.
+        User user = new User();
+        user.setUsername("loda");
+        user.setPassword(passwordEncoder.encode("loda"));
+        userRepository.save(user);
+        System.out.println(user);
     }
 }
